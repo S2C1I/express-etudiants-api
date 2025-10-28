14ca4a626a05787a2310e738db4e5ccc1959b62f
Touhami Amine <74462199+S2C1I@users.noreply.github.com>
2025-10-28 15:16:50 +0100

    chore: confirm journal controller only persists model info and supports get all

--- Controllers/controllerJournal.js
+++ Controllers/controllerJournal.js
@@ -16,20 +16,6 @@ export async function getAllJournals(req, res, next) {
   }
 }
 
-export async function getJournalById(req, res, next) {
-  try {
-    const journal = await Journal.findById(req.params.id)
-      .populate("userId", "prenom nom email")
-      .populate("etudiantId", "nom prenom email");
-    if (!journal) {
-      return res.status(404).json({ message: "Journal entry not found" });
-    }
-    res.status(200).json(journal);
-  } catch (err) {
-    next(err);
-  }
-}
-
 export async function addJournal(req, res, next) {
   try {
     const { actionType, etudiantId, ipAdress } = req.body;
@@ -47,30 +33,4 @@ export async function addJournal(req, res, next) {
   }
 }
 
-export async function updateJournal(req, res, next) {
-  try {
-    const updateData = req.body;
-    const journal = await Journal.findByIdAndUpdate(req.params.id, updateData, {
-      new: true,
-      runValidators: true,
-    });
-    if (!journal) {
-      return res.status(404).json({ message: "Journal entry not found" });
-    }
-    res.status(200).json(journal);
-  } catch (err) {
-    next(err);
-  }
-}
 
-export async function deleteJournal(req, res, next) {
-  try {
-    const journal = await Journal.findByIdAndDelete(req.params.id);
-    if (!journal) {
-      return res.status(404).json({ message: "Journal entry not found" });
-    }
-    res.status(200).json({ message: "Journal entry deleted", journal });
-  } catch (err) {
-    next(err);
-  }
-}
--- Routes/routerJournal.js
+++ Routes/routerJournal.js
@@ -1,11 +1,5 @@
 import { Router } from "express";
-import {
-  getAllJournals,
-  getJournalById,
-  addJournal,
-  updateJournal,
-  deleteJournal,
-} from "../Controllers/controllerJournal.js";
+import { getAllJournals, addJournal } from "../Controllers/controllerJournal.js";
 import { tokenVerification } from "../Middleware/tokenVerification.js";
 
 const routerJournal = Router();
@@ -14,9 +8,6 @@ const routerJournal = Router();
 routerJournal.use(tokenVerification);
 
 routerJournal.get("/", getAllJournals);
-routerJournal.get("/:id", getJournalById);
 routerJournal.post("/", addJournal);
-routerJournal.put("/:id", updateJournal);
-routerJournal.delete("/:id", deleteJournal);
 
 export default routerJournal;
